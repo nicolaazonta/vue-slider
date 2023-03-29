2e426db33c3ea5e8f2c4e3995a9e6873b387d83c
nicolaazonta <nicolazonta114@gmail.com>
2023-03-29 15:06:06 +0200

    fix and take the right files from old exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,117 @@
-/*
-MILESTONE 1
-Per prima cosa, creiamo il markup statico:
-costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
 
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal.
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-MILESTONE 3
-Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. */
-const album = [
+/* const album = [
     './assets/img/01.webp',
     './assets/img/02.webp',
     './assets/img/03.webp',
     './assets/img/04.webp',
     './assets/img/05.webp'
-]
+] */
+
+const newImages = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
 
-let imagesEl = document.getElementById('contenitore');
 let marker = 2;
 
+//display images into the dom
 
-for (let i = 0 ; i < album.length ; i++){
+newImages.forEach((imageTitleText, index) => {
+    let markup= 
+        
+        `<div class="card-img">
+            <img  class="img-fluid ${index === marker ? 'active' : ''} " src="./assets/${imageTitleText.image}" alt="">
+        </div>
+
+        <div class="card-img-overlay position-absolute bottom-0">
+            <h4 class="${index === marker ? 'active' : ''}">${imageTitleText.title}</h4>
+            <p class="${index === marker ? 'active' : ''} ">${imageTitleText.text}</p>
+        </div>`
+        ;
+
+    document.querySelector('.card').insertAdjacentHTML('afterbegin', markup);  
+});
+
+/* for (let i = 0 ; i < album.length ; i++){
     const imgCurrent = album[i];
-
-    const singleImgElement = `<img  class="img-fluid ${i === marker ? 'active' : ''} " src="${imgCurrent}" alt="">`;
-    
+    const singleImgElement = `<img  class="img-fluid ${i === marker ? 'active' : ''} " src="${imgCurrent}" alt="">`;    
     imagesEl.insertAdjacentHTML('afterbegin' , singleImgElement);
-}
+} */
 
 
-const allImages = document.querySelectorAll('#contenitore > img')  
+const allImages = document.querySelectorAll('.card-img > img');
+const allTitles = document.querySelectorAll('.card > .card-img-overlay > h4');
+const allText = document.querySelectorAll('.card > .card-img-overlay > p');
 
-
+//displau the next image
 const previousButton = document.getElementById('button_up');
 previousButton.addEventListener('click' , function() {
-    console.log('precedente');
 
-    const currentSlide = allImages[marker];
-    console.log(currentSlide + '   44');
-    currentSlide.classList.remove('active');
-
+    console.log('arrowUP');
+    const currentImage = allImages[marker];
+    currentImage.classList.remove('active');
+    const currentTitle = allTitles[marker];
+    currentTitle.classList.remove('active');
+    const currentText = allText[marker];
+    currentText.classList.remove('active'); 
     marker --;
 
     if (marker < 0){
-        marker = album.length - 1;
-    }
+        marker = newImages.length - 1;
+    };
 
+    const prevTitle = allTitles[marker];
+    prevTitle.classList.add('active');
+    const prevText = allText[marker];
+    prevText.classList.add('active');
     const prevImage = allImages[marker];
-
     prevImage.classList.add('active');
 })
 
+//display the previous image
 const nextButton = document.getElementById('button_down');
 nextButton.addEventListener('click' , function() {
-    console.log('seguente');
-    
-    const currentSlide = allImages[marker];
 
-    currentSlide.classList.remove('active');
+    console.log('arrowDOWN');    
+    const currentImage = allImages[marker];
+    currentImage.classList.remove('active');
+    const currentTitle = allTitles[marker];
+    currentTitle.classList.remove('active');
+    const currentText = allText[marker];
+    currentText.classList.remove('active');
 
     marker ++;
-
-    if(marker > album.length - 1){
+    if(marker > newImages.length - 1){
         marker = 0;
-    }
+    };
 
-    const nextImage = allImages[marker];
-
-    nextImage.classList.add('active');
-    
+    const prevTitle = allTitles[marker];
+    prevTitle.classList.add('active');
+    const prevText = allText[marker];
+    prevText.classList.add('active');
+    const prevImage = allImages[marker];
+    prevImage.classList.add('active');    
 })
 
-/* if(marker > album.length){
-    marker = 0;
-} else if (marker < 0){
-    marker = album.length - 1;
-} */
+
 
